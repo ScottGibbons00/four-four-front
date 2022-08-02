2a900aab7a7c8defb826cb8ef9755b553193c9c2
Scott Gibbons <scottgibbons00@gmail.com>
2022-08-01 20:40:03 -0400

    cta styling

--- pages/landing.tsx
+++ pages/landing.tsx
@@ -3,9 +3,9 @@ const Landing = () => {
         <div id='landing'>
            <div id='center-div'>
                <h1>Why settle for anything else?</h1>
-               <div>
-                   <button>Who are we?</button>
-                   <button>Get started</button>
+               <div className='buttons'>
+                   <button id='cta-2'>Who are we?</button>
+                   <button id='cta-1'>Get started</button>
                </div>
            </div>
         </div>
